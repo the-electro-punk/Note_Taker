68924c63e90200f5ab5003b6844c58119a243b09
the-electro-punk <bwr2555@hotmail.com>
2021-10-31 20:21:24 -0500

    fixed code so notes show up

--- ALT/assets/notes.js
+++ ALT/assets/notes.js
@@ -1,31 +1,37 @@
 const newNoteBTN = document.getElementById("new-note")
 
+let noteColumn = document.getElementById("list-group")
 
-const noteColumn = document.getElementById("list-container")
-
+// noteList collects each new note created
 let noteList = []
-let currentNotes = window.storage
-noteColumn.textContent = noteList
+
+// noteColumn.textContent = noteList
 
 newNote = () => {
+    console.log('button pressed')
+    // this collects the data inputted
     let noteTitle = document.getElementById('noteInput')
     let noteText = document.getElementById("noteContent")
     // if (noteTitle.value & noteText.value != null) {
-        document.createElement('p')
-        newNote = noteTitle.value + '\n' + noteText.value
-        noteList.push(newNote)
-        console.log(noteList)
 
+        // this creates an HTML element containing the inputted data
+        if (noteTitle.value != '' && noteText != '') {
+            newNote = noteTitle.value + ':' + '\n' + noteText.value
+            noteList.push(newNote)
+            console.log("note list is " + noteList)
+            noteColumn.textContent = noteList
+            console.log("noteColumn is " + noteColumn)
+        }
 
-        var new_note_data = noteText.value
+        // var new_note_data = noteText.value
         // if(noteList.getItem('data')==null){
         //     noteList.setItem('data', '[]')
         // }
-        var old_note_data = JSON.parse(noteList.getItem('data'))
-        old_note_data.push(new_note_data)
+        // var old_note_data = JSON.parse(noteList.getItem('data'))
+        // old_note_data.push(new_note_data)
 
-        noteList.setItem('data', JSON.stringify(old_note_data))
-        console.log('current storage = ' +window.storage)
+        // noteList.setItem('data', JSON.stringify(old_note_data))
+        // console.log('current storage = ' +window.storage)
 
         // storage()
     // }
